cc8e927df628c5e6b86d28bdf735fc09040a6d42
bikramgiri <asgbrg.github@gmail.com>
2025-06-27 10:27:38 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const app = require('express')()
+// **OR
+// const express = require('express')
+// const app = express()
+
+app.get('/', (req, res)=>{
+    // console.log(req)
+    res.send('I am from Home Page')
+    // res.json({
+    //     message : "Welcome to the Home Page",
+    //     status : "success"
+    // })
+})
+
+app.get('/about', (req, res)=>{
+    res.send('I am from About Page')
+})
+
+app.get('/contact', (req, res)=>{
+    res.send('I am from Contact Page')
+})
+
+const Port  = process.env.PORT || 3000
+app.listen(Port, (req, res)=>{
+    console.log(`Server is running on port ${Port}`)
+})
+
+
