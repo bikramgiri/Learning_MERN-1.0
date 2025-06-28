a91499c2f93cb7c9af84ffc40697d75e0e15cd9d
bikramgiri <asgbrg.github@gmail.com>
2025-06-28 09:22:35 +0545

    JSON, Autromatic server restart, and Test API through Postman

--- app.js
+++ app.js
@@ -6,20 +6,26 @@ const app = require('express')()
 
 app.get('/', (req, res)=>{
     // console.log(req)
-    res.send('I am from Home Page')
-    // res.json({
-    //     message : "Welcome to the Home Page",
-    //     status : "success"
-    // })
+    // res.send('I am from Home Page')
+    res.json({
+        message : "Welcome to the Home Page",
+        status : "success"
+    })
 })
 
 app.get('/about', (req, res)=>{
-    res.send('I am from About Page')
+    // res.send('I am from About Page')
+    res.json({
+        message : "Welcome to the About Page",
+        status : "200"
+    })
 })
 
 app.get('/contact', (req, res)=>{
     res.send('I am from Contact Page')
 })
+
+
 
 const Port  = process.env.PORT || 3000
 app.listen(Port, (req, res)=>{
